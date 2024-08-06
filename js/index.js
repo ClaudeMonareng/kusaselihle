7c47c9d8c541f67b486d4064bf2ffea514b523f9
Lehlohonolo Monareng <43777245+ClaudeMonareng@users.noreply.github.com>
2024-08-06 05:11:51 +0200

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -48,19 +48,19 @@ portfolioCloseBtns.forEach((portfolioCloseBtn) => {
 
 //OUR CLIENTS - SWIPER
 
-var swiper = new Swiper(".clients-swiper", {
-    effect: "coverflow",
-    grabCursor: true,
-    centeredSlides: true,
-    slidesPerView: "auto",
-    coverflowEffect: {
-    rotate: 50,
-    stretch: 0,
-    depth: 100,
-    modifier: 1,
-    slideShadows: true,
-    },
-    pagination: {
-    el: ".swiper-pagination",
-    },
-});
+// var swiper = new Swiper(".sponsors-swiper", {
+//     effect: "coverflow",
+//     grabCursor: true,
+//     centeredSlides: true,
+//     slidesPerView: "auto",
+//     coverflowEffect: {
+//     rotate: 50,
+//     stretch: 0,
+//     depth: 100,
+//     modifier: 1,
+//     slideShadows: true,
+//     },
+//     pagination: {
+//     el: ".swiper-pagination",
+//     },
+// });
