16b4f53ef5e8c74255935609f7ff0b48702a2b58
Lehlohonolo Monareng <43777245+ClaudeMonareng@users.noreply.github.com>
2025-09-01 06:24:00 +0200

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,74 +1,31 @@
 
 // TOGGLE NAVBAR
 
-const menuBtn = document.querySelector(".menu-btn");
-const navigation = document.querySelector(".navigation");
+  const menuBtn = document.querySelector(".nav-menu-btn");
+  const closeBtn = document.querySelector(".nav-close-btn");
+  const navigation = document.querySelector(".navigation");
 
 menuBtn.addEventListener("click", () => {
     menuBtn.classList.toggle("active");
     navigation.classList.toggle("active");
 });
 
-// IMG/VIDEO SLIDER NAVIGATION
+closeBtn.addEventListener("click", () => {
 
-const btns = document.querySelectorAll(".nav-btn");
-const slides = document.querySelectorAll(".image-slide");
-const contents = document.querySelectorAll(".content");
+    navigation.classList.remove("active");
+})
 
+// now, we will fix navigation issue...menu close after clicked any link
 
-var sliderNav = function(manual){
-    
-    btns.forEach((btn) => {
-        btn.classList.remove("active");
-    });
-
-    slides.forEach((slide) => {
-        slide.classList.remove("active");
-    });
-
-    contents.forEach((content) => {
-        content.classList.remove("active");
-    });
-
-    btns[manual].classList.add("active");
-    slides[manual].classList.add("active");
-    contents[manual].classList.add("active");
+function toggleMenu() {
+    menuBtn.classList.remove('active');
+    navigation.classList.remove('active');
 }
 
-btns.forEach((btn, i) => {
-    btn.addEventListener("click", () => {
-        sliderNav(i);
-    });
-});
-
-// STICKY NAVBAR
-// NAVIGATION BAR EFFECTS ON SCROLL
+// STICKY NAVBAR                            
+// NAVIGATION BAR EFFECTS ON SCROLL 
 
 window.addEventListener("scroll", function () {
     const header = document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY > 0);
-});
-
-// PORTFOLIO SECTION - MODAL
-
-const portfolioModals = document.querySelectorAll(".portfolio-modal");
-const imgCards = document.querySelectorAll(".img-card");
-const portfolioCloseBtns= document.querySelectorAll(".portfolio-close-btn");
-
-var portfolioModal = function(modalClick){
-    portfolioModals[modalClick].classList.add("active");
-}
-
-imgCards.forEach((imgCard, i) => {
-    imgCard.addEventListener("click", () => {
-        portfolioModal(i);
-    });
-});
-
-portfolioCloseBtns.forEach((portfolioCloseBtn) => {
-    portfolioCloseBtn.addEventListener("click", () => {
-    portfolioModals.forEach((portfolioModalView) => {
-            portfolioModalView.classList.remove("active");
-        });
-    });
 });
